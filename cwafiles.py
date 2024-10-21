20a00464307e8971b3c5ee13e79acaceeb7a1b9c
Khreight <louisthedeaf@gmail.com>
2024-10-21 22:34:06 +0200

    Update cwafiles.py

--- cwafiles.py
+++ cwafiles.py
@@ -7,40 +7,35 @@ from watchdog.events import FileSystemEventHandler
 class CompileCFilesHandler(FileSystemEventHandler):
     def __init__(self):
         super().__init__()
-        self.modified_files = set()  # Pour suivre les fichiers modifiés
-        self.last_errors = set()  # Pour éviter la répétition des erreurs
-        self.last_modified_time = {}  # Pour stocker le dernier temps de modification
+        self.modified_files = set()
+        self.last_errors = set()
+        self.last_modified_time = {}
 
     def on_modified(self, event):
-        # Vérification des extensions en .c et ignorer la création de fichiers
         if event.src_path.endswith('.c') and event.event_type != 'created':
-            current_time = time.time()  # Obtenir l'heure actuelle
-            # Si le fichier a été modifié récemment, ne rien faire
+            current_time = time.time()
             if event.src_path in self.last_modified_time:
-                if current_time - self.last_modified_time[event.src_path] < 1.0:  # 1 seconde d'intervalle
+                if current_time - self.last_modified_time[event.src_path] < 1.0:
                     return
 
-            self.last_modified_time[event.src_path] = current_time  # Mettre à jour le temps de modification
+            self.last_modified_time[event.src_path] = current_time
 
-            # Vérification de la taille du fichier pour s'assurer qu'il n'est pas vide
+
             if os.path.getsize(event.src_path) > 0:
                 print(f"\n   [+] Modification détectée sur le fichier : {event.src_path}")
-                time.sleep(0.5)  # Ajoute un délai avant la compilation
+                time.sleep(0.5)
                 self.compile_file(event.src_path)
             else:
                 print(f"   [-] Ignorer le fichier vide : {event.src_path}")
 
     def compile_file(self, file_path):
-        # Récupérer le répertoire et le nom du fichier
         directory = os.path.dirname(file_path)
         file_name = os.path.basename(file_path)
         executable_name = os.path.splitext(file_name)[0] + '.exe'
         obj_name = os.path.splitext(file_name)[0] + '.obj'
         
-        # Commande pour compiler le fichier avec Visual Studio (cl)
         compile_command = f'cl "{file_path}" /Fe"{os.path.join(directory, executable_name)}" /Fo"{os.path.join(directory, obj_name)}"'
         
-        # Exécuter la commande dans le prompt de Visual Studio
         process = subprocess.Popen(compile_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
 
@@ -51,17 +46,15 @@ class CompileCFilesHandler(FileSystemEventHandler):
             self.display_errors(stdout.decode('cp1252', errors='replace'))
 
     def display_errors(self, error_output):
-        error_lines = error_output.splitlines()  # Sépare les lignes
+        error_lines = error_output.splitlines()
 
-        if error_lines:  # Vérifie si il y a des lignes d'erreur
+        if error_lines:
             for line in error_lines:
-                # Remplacement des caractères spéciaux
                 line = self.replace_special_characters(line)
 
-                # Affichage formaté des erreurs
                 if line not in self.last_errors:
-                    self.last_errors.add(line)  # Ajoute la ligne aux erreurs affichées
-                    print(f"       └-> {line}")  # Indentation pour une meilleure lisibilité
+                    self.last_errors.add(line)
+                    print(f"       └-> {line}")
         else:
             # Si aucune ligne d'erreur n'est trouvée
             print("       └-> [!] Aucun message d'erreur pertinent trouvé.")
